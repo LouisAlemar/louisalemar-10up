4fd47141122c5dc8ba30ed840d9c495b893047d9
Louis Alemar <louisalemar@gmail.com>
2023-09-10 12:07:16 -0400

    create post item component and style posts listing

---
+++ src/components/PostItem/index.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import Image from "next/image";
+import DOMPurify from 'dompurify';
+import { Comic_Neue } from 'next/font/google'
+
+import { Post } from "@/features/posts/post.type";
+
+const comicNeue = Comic_Neue({ subsets: ['latin'], weight: ["400", "700"] })
+
+export default function PostItem({ post, imageThumbnail }: { post: Post, imageThumbnail: string | undefined }) {
+  return (
+    <div className='post-item' key={post.id}>
+      <Link href={`/posts/${post.slug}`} >
+        <div className='image-container'>
+          <Image src={imageThumbnail as string} width={150} height={150} alt='' />
+        </div>
+        <div className='content-container'>
+          <h3 className={comicNeue.className}>{post.title.rendered}</h3>
+          <p
+            dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(post.content.rendered) }}
+          ></p>
+        </div>
+      </Link>
+    </div>
+  );
+}
